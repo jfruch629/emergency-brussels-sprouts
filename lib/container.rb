ef16d2470b06ce2430d084ac5e832aed979dbc50
Jake Fruci <jfruch629@gmail.com>
2017-08-28 17:33:14 -0400

    All tests pass

--- lib/container.rb
+++ lib/container.rb
@@ -1,10 +1,12 @@
 class Container
-  attr_reader :weight, :maximum_holding_weight, :ingredients
+  attr_reader :weight, :maximum_holding_weight, :ingredients, :count, :ingredient_count
 
   def initialize(weight, maximum_holding_weight)
     @weight = weight
     @maximum_holding_weight = maximum_holding_weight
     @ingredients = []
+    @weight_count = 0
+    @ingredient_count = 0
   end
 
   def which_ingredient
@@ -12,10 +14,27 @@ class Container
   end
 
   def fill_with_ingredient(ingredient)
-    ingredients << ingredient
+    @weight_count = @weight
+
+    while @weight_count < @maximum_holding_weight + @weight
+      ingredients << ingredient
+      @weight_count += ingredient.weight
+      @ingredient_count += 1
+    end
+    @weight = @weight_count.round
   end
 
   def how_many_ingredients
-    ingredients.count
+    @ingredient_count
+  end
+
+  def remove_one_ingredient
+    ingredients.pop
+    @ingredient_count -= 1
+  end
+
+  def empty
+    ingredients.clear
+    @ingredient_count = 0
   end
 end
